240f0d16c2ae8cecfd9754f8a56400dcca0142fd
Guilherme Almeida <gui.costa123@hotmail.com>
2019-06-03 03:34:09 -0300

    new project vue

---
+++ dist/precache-manifest.3fe28da1094ad80a7c1b032d91440c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "66af40519fa91897e278",
+    "url": "/js/chunk-vendors.2de3957b.js"
+  },
+  {
+    "revision": "9d9c04f51686930977e7",
+    "url": "/js/app.d43ac811.js"
+  },
+  {
+    "revision": "aedebbfb6fd1b72e7837ea6653b96414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66af40519fa91897e278",
+    "url": "/css/chunk-vendors.8942874e.css"
+  },
+  {
+    "revision": "9d9c04f51686930977e7",
+    "url": "/css/app.c2f2773a.css"
+  }
+];
